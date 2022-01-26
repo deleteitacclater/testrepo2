f9c31fe5e79cf0ce6aa5906fe82985f9ebc1c2b1
deleteitacclater <deadfighter96@mail.ru>
2022-01-27 02:37:48 +0400

    add class in obsolete branch

---
+++ GitTest3/SomeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest3_Rename
+{
+    class SomeClass
+    {
+        //new class in the obsolete branch
+    }
+}
